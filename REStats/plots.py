6a1999b36359cd88b33b6cb9249b572e8cae7e3c
Cameron Irmas <cirmas@protonmail.com>
2023-04-28 14:54:22 -0700

    Add windrose

--- REStats/plots.py
+++ REStats/plots.py
@@ -1,5 +1,8 @@
 import os
 
+from windrose import WindroseAxes
+import matplotlib.pyplot as plt
+
 
 def save_figs(figs, format="pdf", output_dir=None):
     curr_dir = os.path.dirname(os.path.realpath(__file__))
@@ -9,3 +12,32 @@ def save_figs(figs, format="pdf", output_dir=None):
     for name in figs:
         outfile = f"{output_dir}/{name}.{format}"
         figs[name].savefig(outfile, bbox_inches="tight")
+
+
+def plot_wind_rose(df, speed_col="wind_speed", direction_col="wind_dir", bins=None, cmap=None, legend=True, **kwargs):
+    """
+    Plot a wind rose using a pandas DataFrame and return the figure object.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame containing wind speed and direction data.
+    speed_col (str): Column name for wind speed data.
+    direction_col (str): Column name for wind direction data.
+    bins (list or int, optional): Number of bins or a list of bins for wind speed data. Defaults to None.
+    cmap (list, optional): Colormap for the plot.
+    legend (bool, optional): Whether to display the legend. Defaults to True.
+    
+    Returns:
+    matplotlib.figure.Figure: The generated figure object.
+    """
+    # Create a new figure and windrose axes
+    fig = plt.figure(**kwargs)
+    ax = WindroseAxes.from_ax(fig=fig)
+    
+    # Create the wind rose plot
+    ax.bar(df[direction_col], df[speed_col], normed=True, bins=bins, cmap=cmap)
+
+    # Show legend
+    if legend:
+        ax.set_legend()
+
+    return fig
